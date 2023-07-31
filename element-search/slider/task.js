f10c3faacb55dba15e53e12e2ff15e9d70c97fa0
Сергей Нехайчик <byrbka@yandex.ru>
2023-07-31 13:09:18 +0100

    Задание выполнено и изменено

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,18 +1,21 @@
 const allSlides = Array.from(document.querySelectorAll(".slider__item")); 
-let currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
-
 const slidePrev = document.querySelector('.slider__arrow_prev');
 const slideNext = document.querySelector('.slider__arrow_next');
 
+//const currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
+
 function nextSlide() {
+    let currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
     goToSlide(currentSlide + 1);
 }
 
 function previousSlide() {
+    let currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
     goToSlide(currentSlide - 1);
 }
 
 function goToSlide(n) {
+    let currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
     allSlides[currentSlide].classList.remove('slider__item_active')    
     currentSlide = (n + allSlides.length) % allSlides.length;       
     allSlides[currentSlide].classList.add('slider__item_active')    
