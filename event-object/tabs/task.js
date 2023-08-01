07f8a4366a5f6d9fe87381667103826e8a0adc1b
Сергей Нехайчик <byrbka@yandex.ru>
2023-08-01 17:27:45 +0100

    Задание выполнено

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+const tabs = Array.from(document.querySelectorAll('.tab'))
+const tabsCurent = Array.from(document.querySelectorAll('.tab__content'))
+tabs.forEach((el, idx) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+        tabs.forEach((elem) => {
+            elem.classList.remove('tab_active')
+        })
+        el.classList.add('tab_active')
+        tabsCurent.forEach((elem) => {
+            elem.classList.remove('tab__content_active')
+        })
+        tabsCurent[idx].classList.add('tab__content_active')
+    })
+})
+
