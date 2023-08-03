d4e9e91363e7e4c2892cef562b965d1cf7156943
Сергей Нехайчик <byrbka@yandex.ru>
2023-08-03 21:23:45 +0100

    Задание выполнено c исправлением

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,4 +1,4 @@
-const btn = document.querySelectorAll('.font-size') // кнопки
+const btn = document.querySelectorAll('.font-size') 
 btn.forEach((e) => {
     e.addEventListener('click', (el) => {
         el.preventDefault();
@@ -7,12 +7,15 @@ btn.forEach((e) => {
         e.classList.add('font-size_active');
         if (e.classList.contains('font-size_small')){
             book.classList.add('book_fs-small');
+            book.classList.remove('book_fs-big');
         } else if (e.classList.contains('font-size_big')) {
             book.classList.add('book_fs-big');
+            book.classList.remove('book_fs-small');
         } else {
             book.classList.remove('book_fs-small','book_fs-big');
         }
     })
 })
+
 
 
